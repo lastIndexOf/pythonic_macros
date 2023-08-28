05ed3fc79599af4a6c631e62c95310cedcd107d0
cold_sharp <kgaikj2cu@icloud.com>
2023-08-28 22:56:38 +0800

    chore: pythonic vec in rust

---
+++ src/vec.rs
@@ -0,0 +1,31 @@
+pub macro p_vec {
+    ($ee:expr; for $ele:ident in $pp:expr) => {
+        {
+            let mut arr = vec![];
+
+            for $ele in $pp {
+                arr.push($ee);
+            }
+
+            arr
+        }
+    },
+    () => {}
+}
+
+#[cfg(test)]
+mod test_p_vec {
+    use super::*;
+
+    #[test]
+    fn test_p_vec_with_for_loop() {
+        assert_eq!(p_vec![i; for i in 0..5], [0, 1, 2, 3, 4]);
+        assert_eq!(p_vec![i * 2; for i in 0..5], [0, 2, 4, 6, 8]);
+        assert_eq!(p_vec![i * i; for i in 0..5], [0, 1, 4, 9, 16]);
+    }
+
+    #[test]
+    fn test_p_vec_with_two_layers_for_loop() {
+        // TODO
+    }
+}
