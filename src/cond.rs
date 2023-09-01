eb432e780bff3c983dd1a31a01230bf060da64b0
cold_sharp <kgaikj2cu@icloud.com>
2023-09-02 01:43:57 +0800

    chore: optz pythonic condition

--- src/cond.rs
+++ src/cond.rs
@@ -1,53 +1,8 @@
 pub macro p_cond {
     () => {},
-    ($true_value:tt if $cond:tt else $false_value:expr) => {
+    ($true_value:expr; if $cond:expr; else $false_value:expr) => {
         {
             if $cond {
-                $true_value
-            } else {
-                $false_value
-            }
-        }
-    },
-    ($true_value:tt if $cond_left:tt == $cond_right:tt else $false_value:expr) => {
-        {
-            if $cond_left == $cond_right {
-                $true_value
-            } else {
-                $false_value
-            }
-        }
-    },
-    ($true_value:tt if $cond_left:tt > $cond_right:tt else $false_value:expr) => {
-        {
-            if $cond_left > $cond_right {
-                $true_value
-            } else {
-                $false_value
-            }
-        }
-    },
-    ($true_value:tt if $cond_left:tt >= $cond_right:tt else $false_value:expr) => {
-        {
-            if $cond_left >= $cond_right {
-                $true_value
-            } else {
-                $false_value
-            }
-        }
-    },
-    ($true_value:tt if $cond_left:tt < $cond_right:tt else $false_value:expr) => {
-        {
-            if $cond_left < $cond_right {
-                $true_value
-            } else {
-                $false_value
-            }
-        }
-    },
-    ($true_value:tt if $cond_left:tt <= $cond_right:tt else $false_value:expr) => {
-        {
-            if $cond_left <= $cond_right {
                 $true_value
             } else {
                 $false_value
@@ -62,10 +17,10 @@ mod test_p_cond {
 
     #[test]
     fn test_p_cond() {
-        assert_eq!(10, p_cond!(10 if 10 == 10 else 20));
-        assert_eq!(20, p_cond!(10 if 10 == 20 else 20));
-        assert_eq!(10, p_cond!(10 if 10 >= 10 else 20));
-        assert_eq!(10, p_cond!(10 if 10 > 0 else 20));
-        assert_eq!(10, p_cond!((2 * 5) if 10 > 0 else 20));
+        assert_eq!(20, p_cond!(2 * 5; if 10 * 2 == 10; else 20));
+        assert_eq!(20, p_cond!(2 * 5; if 10 == 20; else 20));
+        assert_eq!(10, p_cond!(2 * 5; if 10 >= 10; else 20));
+        assert_eq!(10, p_cond!(2 * 5; if 10 > 0; else 20));
+        assert_eq!(10, p_cond!(2 * 5; if 10 > 0; else 20));
     }
 }
